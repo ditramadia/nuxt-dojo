577eb00b2eb4c83b9083e3d3703d24edcd6e4520
ditramadia <ditrarizqaamadia@gmail.com>
2024-07-01 16:17:41 +0700

    feat: add home page

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, voluptates.</p>
+  </div>  
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+  h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+  }
+  
+  p {
+    margin: 20px 0;
+  }
+</style>
