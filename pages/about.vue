924d6074f6a0d68f4c2467315982e700b915551d
Ditra Amadia <60308058+ditramadia@users.noreply.github.com>
2024-07-01 16:30:44 +0700

    feat: add pages
    * feat: add home page
    
    * feat: add product page
    
    * feat: add product page

---
+++ pages/about.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>About</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, voluptates.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, voluptates.</p>
+  </div>  
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+  h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+  }
+  
+  p {
+    margin: 20px 0;
+  }
+</style>
