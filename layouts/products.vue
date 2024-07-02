4193f8d2aa56f3faa892588de870707e4710aea7
ditramadia <ditrarizqaamadia@gmail.com>
2024-07-02 08:36:53 +0700

    feat: add tailwind css

--- layouts/products.vue
+++ layouts/products.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
-    <header>
-      <nav>
-        <NuxtLink to="/products">Nuxt Dojo Merch</NuxtLink>
+    <header class="bg-white shadow-sm">
+      <nav class="container mx-auto p-4">
+        <NuxtLink to="/products" class="font-bold">Nuxt Dojo Merch</NuxtLink>
       </nav>
     </header>
 
-    <div>
+    <div class="container mx-auto p-4">
       <slot />
     </div>
 
-    <footer>
-      <ul>
+    <footer class="container mx-auto p-4 flex justify-between">
+      <ul class="flex gap-4">
         <li><NuxtLink to="/">Home</NuxtLink></li>
         <li><NuxtLink to="/about">About</NuxtLink></li>
         <li><NuxtLink to="/products">Products</NuxtLink></li>
