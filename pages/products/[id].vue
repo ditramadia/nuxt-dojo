243d95537238c48c8fe3faacb520560571405bdb
ditramadia <ditrarizqaamadia@gmail.com>
2024-07-02 09:27:01 +0700

    feat: add error page

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -12,4 +12,8 @@
   const { id } = useRoute().params;
   const uri = `https://fakestoreapi.com/products/${id}`;
   const { data: product } = await useFetch(uri, { key: id });
+
+  if (!product.value) {
+    throw createError({ statusCode: 404, message: 'Product not found'}, fatal: true);
+  }
 </script>
